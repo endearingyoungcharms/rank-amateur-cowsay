36b1fc508e2bd85e3dcbb695ddbe61ccb8fbadfd
Jesse Callaway <jessehub@users.noreply.github.com>
2019-12-04 12:17:00 -0800

    Update formula.rb

--- formula.rb
+++ formula.rb
@@ -2,7 +2,8 @@ class Cowsay < Formula
   desc "Configurable talking characters in ASCII art"
   # Historical homepage: https://web.archive.org/web/20120225123719/www.nog.net/~tony/warez/cowsay.shtml
   # Based on Formula "https://github.com/tnalpgge/rank-amateur-cowsay"
-  url ""
+  url "https://github.com/jessehub/homebrew-cowsay-looker/archive/cowsay-looker-0.1.tar.gz"
+  sha256 "963eea5551f92c5b8a9c12283d23f53840a2cd4d78fdf35c553038202a4e0f16"
   
   def install
     system "/bin/sh", "install.sh", prefix
