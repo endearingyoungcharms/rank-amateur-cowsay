998640ee1c9d1fb569ac3e40e2b7fda5589514c1
Jesse Callaway <jessehub@users.noreply.github.com>
2019-12-04 06:31:08 -0800

    Create formula.rb

---
+++ formula.rb
@@ -0,0 +1,16 @@
+class Cowsay < Formula
+  desc "Configurable talking characters in ASCII art"
+  # Historical homepage: https://web.archive.org/web/20120225123719/www.nog.net/~tony/warez/cowsay.shtml
+  # Based on Formula "https://github.com/tnalpgge/rank-amateur-cowsay"
+
+  def install
+    system "/bin/sh", "install.sh", prefix
+    mv prefix/"man", share
+  end
+
+  test do
+    output = shell_output("#{bin}/cowsay moo")
+    assert_match "moo", output  # bubble
+    assert_match "^__^", output # cow
+  end
+end
